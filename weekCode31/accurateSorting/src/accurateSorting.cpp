24f85d2fadb89c22dba7fd634ca6540eb2ba69b1
Gabriel Becker <gabrieloliveirabecker@gmail.com>
2017-04-11 12:57:28 -0300

    [issue #2] Accurate Sorting
    Fisrt commit

---
+++ weekCode31/accurateSorting/src/accurateSorting.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int a_i = 0; a_i < n; a_i++){
+           cin >> a[a_i];
+        }
+        // Write Your Code Here
+    }
+    return 0;
+}
+
